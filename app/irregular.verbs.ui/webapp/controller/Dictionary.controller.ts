83a6e543b5dd33a6e3ae54743affebf8537b184a
Yaroslav-BV <yaroslav.badikov@yandex.ru>
2023-08-12 23:59:20 +0400

    Added test dictionary component

---
+++ app/irregular.verbs.ui/webapp/controller/Dictionary.controller.ts
@@ -0,0 +1,10 @@
+import BaseController from "./BaseController";
+
+/**
+ * @namespace irregular.verbs.ui.controller
+ */
+export default class Dictionary extends BaseController {
+	onInit(): void {
+		console.log("Это словарь");
+	}
+}
