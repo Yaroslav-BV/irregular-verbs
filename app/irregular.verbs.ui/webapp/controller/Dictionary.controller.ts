feb50f1ce3522e77d3722bdc6faaa7cdd2daf031
Yaroslav-BV <yaroslav.badikov@yandex.ru>
2023-08-14 13:10:00 +0400

    Added search verbs

--- app/irregular.verbs.ui/webapp/controller/Dictionary.controller.ts
+++ app/irregular.verbs.ui/webapp/controller/Dictionary.controller.ts
@@ -1,10 +1,14 @@
 import BaseController from "./BaseController";
 import JSONModel from "sap/ui/model/json/JSONModel";
-import MessageBox from "sap/m/MessageBox";
-import MessageToast from "sap/m/MessageToast";
 import ODataListBinding from "sap/ui/model/odata/v4/ODataListBinding";
 import ResourceModel from "sap/ui/model/resource/ResourceModel";
 import ResourceBundle from "sap/base/i18n/ResourceBundle";
+import Filter from "sap/ui/model/Filter";
+import FilterOperator from "sap/ui/model/FilterOperator";
+import FilterType from "sap/ui/model/FilterType";
+import MessageBox from "sap/m/MessageBox";
+import MessageToast from "sap/m/MessageToast";
+import SearchField from "sap/m/SearchField";
 import { IDictionaryStore } from "../interfaces/dictionary.interfaces";
 import { DICTIONARY_STORE_NAME } from "../constants/dictionary.constant";
 
@@ -42,6 +46,24 @@ export default class Dictionary extends BaseController {
 			"/editable",
 			false
 		);
+	}
+
+	onSearch(): void {
+		const searchField = this.byId("searchField") as SearchField;
+		const searchValue = searchField.getValue();
+
+		const filters = new Filter([
+			new Filter("base", FilterOperator.Contains, searchValue),
+			new Filter("past", FilterOperator.Contains, searchValue),
+			new Filter("participle", FilterOperator.Contains, searchValue),
+			new Filter("translation", FilterOperator.Contains, searchValue),
+		]);
+
+		const tableBinding = this.byId("dictionaryList").getBinding(
+			"items"
+		) as ODataListBinding;
+
+		tableBinding.filter(filters, FilterType.Application);
 	}
 
 	onRefresh(): void {
